0818cd95467efc768032867054208a6adae8020f
Liam Wedge <21100218@virtualwindow.co.za>
2022-03-15 15:00:33 +0200

    Styling and navigation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,10 @@ function Home() {
   return (
     <div className='home'>
       <h1>Home page</h1>
+      
+      <div className='welcome'>
+        <h3>Welcome to Algo crypt, we offer the latest up to date information on all things crypto</h3>
+      </div>
     </div>
   )
 }
