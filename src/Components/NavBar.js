2fa2eeda668e57068126625e6a9491f63dcdbb70
Liam Wedge <21100218@virtualwindow.co.za>
2022-08-31 22:19:03 +0200

    updated nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import Home from './Home';
-import Comparison from './Comparison';
-import Timeline from './Timeline';
-import {Link} from 'react-router-dom';
-
+import React from "react";
+import Home from "./Home";
+import Comparison from "./Comparison";
+import Timeline from "./Timeline";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className='NavBar'>
+    <nav className="NavBar">
       <h1>ALGO CRYPT</h1>
-      <img src='./unnamed.jpg'></img>
-      <div className='links'>
-        
+      <img src="./unnamed.jpg"></img>
+      <div className="links">
         <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/Comparison'>Comparison</Link></li>
-          <li><Link to='/Timeline'>Timeline</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Comparison">Comparison</Link>
+          </li>
+          <li>
+            <Link to="/Timeline">Timeline</Link>
+          </li>
         </ul>
-        
-      
       </div>
-
-
     </nav>
-  )
+  );
 }
 
 export default NavBar;
